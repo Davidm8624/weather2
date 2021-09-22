e7e925dc646b285a1e98f2912a75bfde84b46633
David M <davidm8624@gmail.com>
2021-09-22 15:13:44 -0600

    prob going to change everything later tonight

--- weather2/src/App.js
+++ weather2/src/App.js
@@ -1,13 +1,7 @@
-import Card from './components/Cards';
-
 
 function App() {
   return (
     <div>
-      <h1>days</h1>
-      <Card text='day 1'/>
-      <Card text='2'/>
-      <Card text='3'/>
 
     </div>
   );
