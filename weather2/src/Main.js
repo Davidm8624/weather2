6de1c2109e0c1719b9f1b1b4ae233cee568ce2c7
David M <davidm8624@gmail.com>
2021-09-24 13:54:45 -0600

    just needs styling

--- weather2/src/Main.js
+++ weather2/src/Main.js
@@ -8,46 +8,79 @@ const api = {
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
-
 function Main() {
-const [query, setQuery] = useState("");
-const [weather, setWeather] = useState({});
-const search = (e) => {
-  if (e.key === "Enter") {
-    fetch(`${api.enpoint}forecast?q=${query}&units=imperial&appid=${api.key}`)
-      .then((data) => data.json())
-      .then((result) => {
-        setQuery("");
-        setWeather(result);
-        console.log(result);
-      });
-  }
-};
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.enpoint}forecast?q=${query}&units=imperial&appid=${api.key}`)
+        .then((data) => data.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
 
-return (
-  <div>
-    <main>
-      <div>
-        <input
-          type="text"
-          placeholder="Location's name"
-          className="search-bar"
-          onChange={(e) => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        />
-      </div>
-      {typeof weather.list != "undefined" ? (
+  return (
+    <div>
+      <main>
         <div>
-            <div className="currentDay">
-            {weather.list[0].main.temp} F
-              </div>
+          <input
+            type="text"
+            placeholder="Location's name"
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-      ) : (
-        "No input or Location does not exist"
-      )}
-    </main>
-  </div>
-);
-      }
+        {typeof weather.list != "undefined" ? (
+          <div>
+            <div className="currentDay">
+              <div>{weather.city.name} { weather.city.country}</div>
+              <div>{weather.list[0].main.temp} F</div>
+              <div>Feels like: {weather.list[0].main.feels_like} F</div>
+              <div>Humidity: {weather.list[0].main.humidity}</div>
+              <div>Wind speed: {weather.list[0].wind.speed} mph</div>
+              <div>Amount of sky covered in {weather.list[0].weather[0].description}: {weather.list[0].clouds.all}%</div>
+              {/* <div>Type of clouds: {weather.list[0].weather[0].description}</div> */}
+            </div>
+            <div className="tommorow">
+              <div>{weather.list[1].main.temp} F</div>
+              <div>Feels like: {weather.list[1].main.feels_like} F</div>
+              <div>Humidity: {weather.list[1].main.humidity}</div>
+              <div>Wind speed: {weather.list[1].wind.speed} mph</div>
+              <div>Amount of sky covered in {weather.list[1].weather[0].description}: {weather.list[1].clouds.all}%</div>
+            </div>
+            <div className="theDayAfterThat">
+              <div>{weather.list[2].main.temp} F</div>
+              <div>Feels like: {weather.list[2].main.feels_like} F</div>
+              <div>Humidity: {weather.list[2].main.humidity}</div>
+              <div>Wind speed: {weather.list[2].wind.speed} mph</div>
+              <div>Amount of sky covered in {weather.list[2].weather[0].description}: {weather.list[2].clouds.all}%</div>
+            </div>
+            <div className="5daysFromNegative2DaysInThePast">
+              <div>{weather.list[3].main.temp} F</div>
+              <div>Feels like: {weather.list[3].main.feels_like} F</div>
+              <div>Humidity: {weather.list[3].main.humidity}</div>
+              <div>Wind speed: {weather.list[3].wind.speed} mph</div>
+              <div>Amount of sky covered in {weather.list[3].weather[0].description}: {weather.list[3].clouds.all}%</div>
+            </div>
+            <div className="4intoTheFuture">
+              <div>{weather.list[4].main.temp} F</div>
+              <div>Feels like: {weather.list[4].main.feels_like} F</div>
+              <div>Humidity: {weather.list[4].main.humidity}</div>
+              <div>Wind speed: {weather.list[4].wind.speed} mph</div>
+              <div>Amount of sky covered in {weather.list[4].weather[0].description}: {weather.list[4].clouds.all}%</div>
+            </div>
+          </div>
+        ) : (
+          "No input or Location does not exist"
+        )}
+      </main>
+    </div>
+  );
+}
 export default Main;
